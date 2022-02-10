1edb2ffd0d82a833304aed8c020075148e832972
Juwon Oh <13a71032776@gmail.com>
2022-02-10 15:08:34 +0900

    feat: add whitehouse scraper

---
+++ crawler/whitehouse_scraper/whitehouse_scraper/__init__.py
@@ -0,0 +1,3 @@
+from .parser import parse_page
+from .scraper import get_allnews_urls, get_last_page_num, yield_latest_allnews
+from .utils import get_soup, news_dateformat, strf_to_datetime, user_dateformat
---
+++ crawler/whitehouse_scraper/whitehouse_scraper/parser.py
@@ -0,0 +1,53 @@
+import re
+
+from dateutil.parser import parse
+
+from .utils import get_soup, now
+
+
+def parse_page(url):
+    """
+    Argument
+    --------
+    url : str
+        Web page url
+
+    Returns
+    -------
+    json_object : dict
+        JSON format web page contents
+        It consists with
+            title : article title
+            time : article written time
+            content : text with line separator \\n
+            url : web page url
+            scrap_time : scrapped time
+    """
+
+    try:
+        soup = get_soup(url)
+        title = soup.find("h1", class_="page-title topper__title news").text
+        title = re.sub("\n", "", title, 100).replace("\u202f", " ")
+        title = re.sub("\t", "", title, 100).replace("\xa0", " ")
+        date = soup.find(
+            "time", class_="posted-on entry-date published updated"
+        ).text
+        date = parse(date).strftime("%Y-%m-%d")
+        phrases = soup.find("section", class_="body-content").find_all("p")
+        content = "\n".join([p.text.strip() for p in phrases])
+        content = re.sub("\n", "", content, 100)
+        content = re.sub("\t", "", content, 100)
+        content = re.sub("\xa0", "", content, 1000).replace("\u202f", " ")
+
+        json_object = {
+            "title": title,
+            "date": date,
+            "content": content,
+            "url": url,
+            "scrap_time": now(),
+        }
+        return json_object
+    except Exception as e:
+        print(e)
+        print("Parsing error from {}".format(url))
+        return None
