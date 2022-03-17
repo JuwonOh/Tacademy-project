31197e138d4ab5ac8a9258ecc7a2ccab46e37051
Juwon Oh <13a71032776@gmail.com>
2022-03-17 17:10:14 +0900

    feat: add api serving preprocess module

--- app/worker/predict.py
+++ app/worker/predict.py
@@ -5,12 +5,13 @@ import numpy as np
 import pandas as pd
 import torch
 from mlflow.tracking import MlflowClient
-from torch import nn
-from transformers import MobileBertModel, MobileBertTokenizer
+from preprocessing import morethan_two_countries
+from transformers import AutoTokenizer
 
 import mlflow
 
 
+# juwon: Predict의 존재 이유를 모르겠음. 개인적으로는 삭제하는게 맞다고 생각함.
 class Predict:
     def predict(input_text, PRE_TRAINED_MODEL_NAME, model_name):
         """
@@ -111,9 +112,20 @@ if __name__ == "__main__":
 
     def predicting(input_text: str, PRE_TRAINED_MODEL_NAME, model_name):
         # input_text = "President Joe Biden must take expeditious and decisive action immediately against the Russian Federation. The President must order all Russian and civilians to lay down their arms and surrender."
-        class_prob, pred = inference_sentence(
-            input_text, PRE_TRAINED_MODEL_NAME, model_name
-        )
+        valid, related_nation = morethan_two_countries(input_text)
+        if valid:
+            class_prob, pred = inference_sentence(
+                input_text, PRE_TRAINED_MODEL_NAME, model_name
+            )
+            relation_dict = {"0": "나쁨", "1": "좋음"}
+            relation = pred.map(relation_dict)
+            print(
+                "이 문장은 {}사이의 관계에 대한 문장입니다. 이 문장에서는 {}의 관계가 {}다고 예측합니다.".format(
+                    related_nation, related_nation, relation
+                )
+            )
+        else:
+            print("이 문장은 국가간 관계를 살펴보기에 맞는 문장이 아닙니다. 국가가 2개 언급된 다른 문장을 넣어주세요.")
         return (class_prob, pred)
 
     from line_profiler import LineProfiler
